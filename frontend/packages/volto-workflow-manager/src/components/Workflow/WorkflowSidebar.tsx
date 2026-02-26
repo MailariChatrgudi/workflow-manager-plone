c50282073fad139d52cd917f75294f56e6a5f6fa
Mailari-Chatragudi <mailarichatrgudi@gmail.com>
2026-02-26 20:18:05 +0530

    sidebar color changed

--- frontend/packages/volto-workflow-manager/src/components/Workflow/WorkflowSidebar.tsx
+++ frontend/packages/volto-workflow-manager/src/components/Workflow/WorkflowSidebar.tsx
@@ -154,6 +154,7 @@ const Sidebar = (props) => {
           key: 'transitions',
           title: intl.formatMessage(messages.transitions),
           content: (
+            
             <Transition
               workflowId={currentWorkflow.id}
               workflow={currentWorkflow}
@@ -190,7 +191,7 @@ const Sidebar = (props) => {
       <BodyClass
         className={expanded ? 'has-sidebar' : 'has-sidebar-collapsed'}
       />
-      
+
       <div
         className={cx('sidebar-container', { collapsed: !expanded })}
         style={size > 0 ? { width: size } : null}
